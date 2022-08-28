b7558bba68088c2efdd30103510e71e7177b4dac
ChinyeluLyon <chinyelulyon@gmail.com>
2022-08-28 20:36:23 +0100

    fix all sessions

--- modules/AllSessions/AllSessions.tsx
+++ modules/AllSessions/AllSessions.tsx
@@ -1,18 +1,33 @@
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import useGetParticipants from "../../frontendApi/useGetParticipants";
 import Export from "../Export/Export";
 import ParticipantTable from "../ParticipantTable/ParticipantTable";
 
 type AllSessionsProps = {
-  isOpen:boolean
-}
-const AllSessions = ({isOpen}:AllSessionsProps) => {
+  isOpen: boolean;
+};
+const AllSessions = ({ isOpen }: AllSessionsProps) => {
+  const { fetch } = useGetParticipants();
+  const [participantData, setParticipantData] = useState<Array<Participant>>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await fetch({});
+      setParticipantData(data || []);
+    };
+    if (isOpen) {
+      getData();
+    }
+  }, [isOpen]);
+
   return (
     <div>
       <div>
         <Button>Previous</Button>
         <Button>Next</Button>
       </div>
-      <ParticipantTable isOpen={isOpen} />
+      <ParticipantTable data={participantData || []} />
     </div>
   );
 };
